556abba016d5948363f674f7225467a001d23da4
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-10-17 21:12:00 -0300

    Update Footer.jsx

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,5 +1,4 @@
-import { Text, TextLink } from '@/components/Text';
-import { ThemeSwitcher } from '@/components/ThemeSwitcher';
+import { Text } from '@/components/Text';
 import styles from './Footer.module.css';
 import Spacer from './Spacer';
 import Wrapper from './Wrapper';
@@ -8,14 +7,12 @@ const Footer = () => {
   return (
     <>
       <footer className="uk-section">
-
         <div className="uk-container">
           <Wrapper>
-            <Text className={styles.fim}  color="accents-7">
+            <Text className={styles.fim} color="accents-7">
               Adaptado por Romualdo <span>&#169;</span> 2021
             </Text>
             <Spacer size={2} axis="vertical" />
-            {/* <ThemeSwitcher /> */}
           </Wrapper>
         </div>
       </footer>
