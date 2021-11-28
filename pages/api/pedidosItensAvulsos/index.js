deb9f4562c5293296dcd53a27d65595fe990b3d3
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 19:52:33 -0300

    Update index.js

--- pages/api/pedidosItensAvulsos/index.js
+++ pages/api/pedidosItensAvulsos/index.js
@@ -1,12 +1,12 @@
-import { ValidateProps } from '@/api-lib/constants';
 import { findPedidosItensAvulsos, insertPedidoItensAvulsos } from '@/api-lib/db';
-import { auths, database, validateBody } from '@/api-lib/middlewares';
+import { auths, database } from '@/api-lib/middlewares';
 import { ncOpts } from '@/api-lib/nc';
 import nc from 'next-connect';
 
 const handler = nc(ncOpts);
 
 handler.use(database);
+
 handler.get(async (req, res) => {
   const posts = await findPedidosItensAvulsos(
     req.db,
@@ -14,26 +14,26 @@ handler.get(async (req, res) => {
     req.query.by,
     req.query.limit ? parseInt(req.query.limit, 10) : undefined
   );
-
+  
   res.json({ posts });
 });
+
 handler.post(
   ...auths,
   async (req, res) => {
     if (!req.user) {
       return res.status(401).end();
     }
-
+    
     const post = await insertPedidoItensAvulsos(req.db, {
       nome: req.body.nome,
       ref: req.body.ref,
       add: req.body.add,
       creatorId: req.user._id,
     });
-
+    
     return res.json({ post });
   }
 );
-
 
 export default handler;
