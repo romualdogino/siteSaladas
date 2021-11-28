d2b6f90842c56ec20676f5216928658832283af0
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 19:27:06 -0300

    Update [grupoId].jsx

--- pages/admin/grupos/[grupoId].jsx
+++ pages/admin/grupos/[grupoId].jsx
@@ -4,22 +4,19 @@ import { ViewGrupo } from '@/page-components/Admin/Grupos/view';
 import nc from 'next-connect';
 import Head from 'next/head';
 
-
 export default function UserPostPage({ post }) {
-  console.log(post)
+//   console.log(post);
   if (typeof post.createdAt !== 'string') {
     post.createdAt = new Date(post.createdAt);
   }
   return (
     <>
-    
       <Head>
         <title>
           {post.creator.name} ({post.creator.username}): {post.content}
         </title>
       </Head>
-      
-      <ViewGrupo  post={post} link="grupos" />
+      <ViewGrupo post={post} link="grupos" />
     </>
   );
 }
