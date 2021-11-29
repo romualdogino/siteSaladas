670f04126a37a1d1eea6a8392fe3db6561dcbbe6
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 22:38:06 -0300

    Update index.js

--- pages/api/pedidosItensAvulsos/index.js
+++ pages/api/pedidosItensAvulsos/index.js
@@ -1,4 +1,4 @@
-import { findPedidosItensAvulsos, insertPedidoItensAvulsos } from '@/api-lib/db';
+import { findPedidosItensAvulsos,insertPedidoItensAvulsos } from '@/api-lib/db';
 import { auths, database } from '@/api-lib/middlewares';
 import { ncOpts } from '@/api-lib/nc';
 import nc from 'next-connect';
@@ -6,7 +6,6 @@ import nc from 'next-connect';
 const handler = nc(ncOpts);
 
 handler.use(database);
-
 handler.get(async (req, res) => {
   const posts = await findPedidosItensAvulsos(
     req.db,
@@ -14,24 +13,24 @@ handler.get(async (req, res) => {
     req.query.by,
     req.query.limit ? parseInt(req.query.limit, 10) : undefined
   );
-  
   res.json({ posts });
 });
 
 handler.post(
+  
   ...auths,
+  
   async (req, res) => {
     if (!req.user) {
       return res.status(401).end();
     }
-    
-    const post = await insertPedidoItensAvulsos(req.db, {
-      nome: req.body.nome,
-      ref: req.body.ref,
-      add: req.body.add,
-      creatorId: req.user._id,
-    });
-    
+    const post = await insertPedidoItensAvulsos(
+      req.db, {
+        nome: req.body.nome,
+        ref: req.body.ref,
+        add: req.body.add,
+        creatorId: req.user._id,
+      });
     return res.json({ post });
   }
 );
