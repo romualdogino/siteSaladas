6cc69d0d31af62bf73d87b10a62d93577658b6a4
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 23:07:08 -0300

    Update ItemPedido.jsx

--- components/itemPedido/ItemPedido.jsx
+++ components/itemPedido/ItemPedido.jsx
@@ -1,17 +1,15 @@
-import clsx from 'clsx';
-import { useMemo, useReducer, useRef } from 'react';
-import styles from './Post.module.css';
+import { useRef } from 'react';
 
 const ItemPedido = ({ post, className, valor}) => {
   return (
     <>
       <div id={"ff"+post._id}>
-          <label className="switch">
-              <input id={"gg"+post._id} type="checkbox" />
-              <span className="slider round"></span>
-          </label>
+        <label className="switch">
+          <input id={"gg"+post._id} type="checkbox" />
+          <span className="slider round"></span>
+        </label>
           {valor?<mark><small> <strong>+</strong> r$ {post.add} </small></mark> :""}
-          {post.nome}
+       {post.nome}
       </div>
     </>
   );
