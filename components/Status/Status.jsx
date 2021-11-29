bb6348c4a70079c40e1593ddaa2cdc255dd7cd61
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 21:04:10 -0300

    Update Status.jsx

--- components/Status/Status.jsx
+++ components/Status/Status.jsx
@@ -1,17 +1,8 @@
-import { Avatar } from '@/components/Avatar';
-import { Container } from '@/components/Layout';
-import { format } from '@lukeed/ms';
 import clsx from 'clsx';
-import Link from 'next/link';
 import { useMemo } from 'react';
 import styles from './Post.module.css';
 
 const Status = ({ post, className }) => {
-  const timestampTxt = useMemo(() => {
-    const diff = Date.now() - new Date(post.createdAt).getTime();
-    if (diff < 1 * 60 * 1000) return 'Just now';
-    return `${format(diff, true)} ago`;
-  }, [post.createdAt]);
   return (
     <div className={clsx(styles.root, className)}>
       <div className={styles.wrap}>
