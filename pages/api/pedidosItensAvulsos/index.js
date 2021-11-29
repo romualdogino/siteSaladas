6900f118e009880a28331856061fff8c15c13da8
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 23:21:11 -0300

    Update index.js

--- pages/api/pedidosItensAvulsos/index.js
+++ pages/api/pedidosItensAvulsos/index.js
@@ -1,10 +1,12 @@
-import { findPedidosItensAvulsos,insertPedidoItensAvulsos } from '@/api-lib/db';
+import { 
+  findPedidosItensAvulsos,
+  insertPedidoItensAvulsos 
+} from '@/api-lib/db';
 import { auths, database } from '@/api-lib/middlewares';
 import { ncOpts } from '@/api-lib/nc';
 import nc from 'next-connect';
 
 const handler = nc(ncOpts);
-
 handler.use(database);
 handler.get(async (req, res) => {
   const posts = await findPedidosItensAvulsos(
@@ -15,22 +17,21 @@ handler.get(async (req, res) => {
   );
   res.json({ posts });
 });
-
 handler.post(
-  
   ...auths,
-  
   async (req, res) => {
     if (!req.user) {
       return res.status(401).end();
     }
     const post = await insertPedidoItensAvulsos(
-      req.db, {
+      req.db, 
+      {
         nome: req.body.nome,
         ref: req.body.ref,
         add: req.body.add,
         creatorId: req.user._id,
-      });
+      }
+    );
     return res.json({ post });
   }
 );
