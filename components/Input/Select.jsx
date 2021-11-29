ae059a579f429686286a590dd912e65293b52ac1
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 22:46:48 -0300

    Update Select.jsx

--- components/Input/Select.jsx
+++ components/Input/Select.jsx
@@ -22,21 +22,23 @@ const Select = forwardRef(
       <div className={clsx(styles.root, className)}>
         <label>{label && <div className={styles.label}>{label}</div>}
           <select
-type={htmlType}
-autoComplete={autoComplete}
-placeholder={placeholder}
-ref={ref}
-className={clsx(styles.textarea)}
-aria-label={ariaLabel}
-required={required}>
-  <option value="0" defaultValue>{ firstOption }</option>
-{data.map((e) => {
-  return (<option key={e._id} value={e._id}>{e.nome}</option>)
-})}
-        </select>
-      </label>
-    </div>
-  );
-});
+            type={htmlType}
+            autoComplete={autoComplete}
+            placeholder={placeholder}
+            ref={ref}
+            className={clsx(styles.textarea)}
+            aria-label={ariaLabel}
+            required={required}>
+            <option value="0" defaultValue>{ firstOption }</option>
+            {data.map((e) => {
+                return (<option key={e._id} value={e._id}>{e.nome}</option>)
+              })
+            }
+            </select>
+        </label>
+      </div>
+    );
+  }
+);
 
 export default Select;
