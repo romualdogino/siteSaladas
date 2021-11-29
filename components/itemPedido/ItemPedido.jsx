112058956cd5361a4f049592af28e5168c8ff4b4
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-29 05:57:15 -0300

    Update ItemPedido.jsx

--- components/itemPedido/ItemPedido.jsx
+++ components/itemPedido/ItemPedido.jsx
@@ -1,18 +1,17 @@
-import { useRef } from 'react';
 
-const ItemPedido = ({ post, className, valor}) => {
-  return (
-    <>
-      <div id={"ff"+post._id}>
-        <label className="switch">
-          <input id={"gg"+post._id} type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-          {valor?<mark><small> <strong>+</strong> r$ {post.add} </small></mark> :""}
-       {post.nome}
-      </div>
-    </>
-  );
+const ItemPedido = ({ post, className, valor }) => {
+    return (
+        <>
+            <div id={"ff" + post._id}>
+                <label className="switch">
+                    <input id={"gg" + post._id} type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+                {valor ? <mark><small> <strong>+</strong> r$ {post.add} </small></mark> : ""}
+                {post.nome}
+            </div>
+        </>
+    );
 };
 
 export default ItemPedido;
