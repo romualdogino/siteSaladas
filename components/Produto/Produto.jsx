12562d497de265b999721890fde57e8873b56bea
romualdogino <62469768+romualdogino@users.noreply.github.com>
2021-11-28 22:54:04 -0300

    Update Produto.jsx

--- components/Produto/Produto.jsx
+++ components/Produto/Produto.jsx
@@ -1,9 +1,7 @@
 import clsx from 'clsx';
-import { useMemo } from 'react';
 import styles from './Post.module.css';
 
 const Produto = ({ post, className }) => {
- 
   return (
     <div className={clsx(styles.root, className)}>
       <div className={styles.wrap}>
